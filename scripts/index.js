2da879c2f158b9316d344638aa6965d6927c275b
Alexandr Nazarov <stig8x1@gmail.com>
2025-01-18 14:03:47 +0300

    Первая отправка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
-// @todo: DOM узлы
+initialCards.forEach(function(element) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+    cardElement.remove();
+  });
+  cardElement.querySelector('.card__description');
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__like-button');
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+  placesList.append(cardElement);
+});
